bbfd1a3c9e27aa44b48043379550f15715ff8b4f
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2021-05-21 10:27:22 +0400

    fix in progress

--- SamaraMap/index.php
+++ SamaraMap/index.php
@@ -1,4 +1,26 @@
-
-<? php
-phpinfo();
+<style>
+#footer
+{
+    clear: both;
+    border: 1px groove #aaaaaa;
+    background: gray;
+    color: black;
+    padding: 0;
+    text-align: center;
+    vertical-align: middle;
+    line-height: normal;
+    margin: 0;
+    position: fixed;
+    bottom: 0px;
+    width: 100%;
+}
+</style>
+<?php
+// phpinfo();
 ?>
+<div class="footer" id="footer">
+  <?php
+  echo "PHP version: " . phpversion();
+  ?>
+  
+</div>
