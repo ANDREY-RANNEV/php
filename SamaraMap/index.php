32a8e2fc22b5479274a03ce8f2bef84b483ec380
Andrey Rannev <rannev@fguzsamo.ru>
2021-05-23 13:56:44 +0400

    22

--- SamaraMap/index.php
+++ SamaraMap/index.php
@@ -22,7 +22,7 @@
         border: 1px groove #aaaaaa;
         background: lightgray;
         color: black;
-        /* padding: 0; */
+        padding: 0;
         text-align: center;
         vertical-align: middle;
         line-height: normal;
@@ -48,7 +48,7 @@
         top: 20px;
         /* bottom: 20px; */
         width: 100%;
-        height: 96%;
+        height: 97%;
         left: 0;
         overflow-y: scroll;
         /* scrollbar-color: rebeccapurple green;
@@ -66,52 +66,72 @@
     }
 </style>
 
+<head>
+    <!-- <meta http-equiv="Content-Type" content="text/html" charset="utf-8"> -->
+    <meta http-equiv="Content-Type" content="text/html; charset=windows-1251">
+
+</head>
+
 <body>
-    <!-- <header class=""> -->
     <div class="header" id="header">
         текст<br />ytortuoiertu
     </div>
-    <!-- </header> -->
+
     <div class="content" id="content">
         <?php
         // phpinfo();
-        // $serverName = "[2001:470:1f0b:3f7:5572:aa5a:4c71:a93e]";  
-//         $myServer = "127.0.0.1";
-// $myUser = "sa";
-// $myPass = "sd666999666";
-// $myDB = "vs_new";
-// $dbhandle = sqlsrv_connect($myServer, $myUser, $myPass) or die("Could not connect to database: ".mssql_get_last_message()); 
-$server_name = "localhost";
-$database_name = "vs_new";
 
-try {
-   $conn = new PDO("sqlsrv:Server=$server_name;Database=$database_name;ConnectionPooling=0", "sa", "sd666999666");
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-   echo "work";
-} catch(PDOException $e) {
-    // $e->getMessage();
-    echo "not work</br>";
-    echo $e->getMessage();
-}
+        $myUser = "sa";
+        $myPass = "saadmin";
+        // $database_name = "Went_Test"; 
         // $serverName = "localhost";  
-        // $connectionOptions = array("Database"=>"vs_new"); 
-        // $conn = sqlsrv_connect( $serverName, $connectionInfo);  
-        // echo $conn;
-        // echo "Could  connect.\n"; 
-        // if( $conn === false )  
-        // {  
-        //      echo "Could not connect.\n";  
-        //      die( print_r( sqlsrv_errors(), true));  
-        // }  
+        $server_name = "[2001:470:1f0b:3f7:5572:aa5a:4c71:a93e]";
+        $database_name = "FBUZ";
+        $connectionInfo = array("Database" => $database_name, "UID" => $myUser, "PWD" => $myPass);
+
+        try {
+           $conn = new PDO("sqlsrv:Server=$server_name;Database=$database_name;ConnectionPooling=0", $myUser, $myPass);
+           $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+           $mssqldb_a= "доступна";
+           
+        } catch(PDOException $e) {
+            $mssqldb_a= "не доступна";
+            echo "not work</br>";
+            echo $e->getMessage();
+        }
+
+        $sql = 'SELECT * FROM Users ORDER BY Name';
+        foreach ($conn->query($sql) as $row) {
+            print $row['Name'] . "\t";
+            print $row['Password'] . "\t";
+            print $row['Login'] . "\n";
+        }   
+// $dbhandle = sqlsrv_connect($myServer, $myUser, $myPass);
+// //  or die("Could not connect to database: ".mssql_get_last_message());
+// echo mssql_get_last_message();
         
-              
-            //  die( print_r( sqlsrv_errors(), true));  
- 
+        // $conn = sqlsrv_connect($server_name, $connectionInfo);
+        // if ($conn === false) {
+        //     echo "Could not connect.</br>";
+        //     //  die( print_r( sqlsrv_errors(), true));  
+        //     if (($errors = sqlsrv_errors()) != null) {
+        //         foreach ($errors as $error) {
+        //             echo "SQLSTATE: " . $error['SQLSTATE'] . "<br />";
+        //             echo "code: " . $error['code'] . "<br />";
+        //             echo "message: " . $error['message'] . "<br />";
+        //         }
+        //     }
+        //     $mssqldb_a = "не доступна";
+        // } else {
+        //     $mssqldb_a = "доступна";
+        // }
         ?>
     </div>
+
     <div class="footer" id="footer">
         <?php
-        echo '<a href="https://windows.php.net/" target="_blank">';
+        echo 'Database "' . $database_name . '" ' . $mssqldb_a;
+        echo '&nbsp; <a href="https://windows.php.net/" target="_blank">';
         echo "PHP version: " . phpversion();
         echo '</a> &nbsp;';
         echo '<a href="https://github.com/ANDREY-RANNEV/php.git" target="_blank">';
