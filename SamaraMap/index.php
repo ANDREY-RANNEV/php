271bbdac89c0c463ea3d7c8d591350cb5b583bf2
Andrey Rannev <rannev@fguzsamo.ru>
2021-05-23 11:09:34 +0400

    1

--- SamaraMap/index.php
+++ SamaraMap/index.php
@@ -74,7 +74,39 @@
     <!-- </header> -->
     <div class="content" id="content">
         <?php
-        phpinfo();
+        // phpinfo();
+        // $serverName = "[2001:470:1f0b:3f7:5572:aa5a:4c71:a93e]";  
+//         $myServer = "127.0.0.1";
+// $myUser = "sa";
+// $myPass = "sd666999666";
+// $myDB = "vs_new";
+// $dbhandle = sqlsrv_connect($myServer, $myUser, $myPass) or die("Could not connect to database: ".mssql_get_last_message()); 
+$server_name = "localhost";
+$database_name = "vs_new";
+
+try {
+   $conn = new PDO("sqlsrv:Server=$server_name;Database=$database_name;ConnectionPooling=0", "sa", "sd666999666");
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   echo "work";
+} catch(PDOException $e) {
+    // $e->getMessage();
+    echo "not work</br>";
+    echo $e->getMessage();
+}
+        // $serverName = "localhost";  
+        // $connectionOptions = array("Database"=>"vs_new"); 
+        // $conn = sqlsrv_connect( $serverName, $connectionInfo);  
+        // echo $conn;
+        // echo "Could  connect.\n"; 
+        // if( $conn === false )  
+        // {  
+        //      echo "Could not connect.\n";  
+        //      die( print_r( sqlsrv_errors(), true));  
+        // }  
+        
+              
+            //  die( print_r( sqlsrv_errors(), true));  
+ 
         ?>
     </div>
     <div class="footer" id="footer">
