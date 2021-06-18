c9ff5020f7ed44f00a331810181e4294f54a9cf6
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2021-06-18 17:26:07 +0400

    fix

--- SamaraMap/index.php
+++ SamaraMap/index.php
@@ -29,5 +29,15 @@ if ($sh->num_rows == 1) {
     }
   }
 }
+$table = "Raion";
+$sh = $conn->query("SHOW TABLES LIKE '{$table}'");
+if ($sh->num_rows == 1) {
+  echo "Table '$table' exists</br>";
+} else {
+  echo "Table '$table' does not exist</br>";
+  $sql = "CREATE TABLE `FBUZ`.`$table` ( `id` INT NOT NULL , `id_name` VARCHAR(150) NOT NULL ,`name` VARCHAR(150) NOT NULL ,`cap` VARCHAR(150) NOT NULL , `type` VARCHAR(50) NOT NULL , `strokeStyle` VARCHAR(50) NOT NULL , `fillStyle` VARCHAR(50) NOT NULL , `path` TEXT NOT NULL ) ENGINE = InnoDB;";
+  $sh1 = $conn->query($sql);
+  echo $sh1 === 1;
+}
 $sh->free();
 $conn->close();
