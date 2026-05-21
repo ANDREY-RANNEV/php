a6d7478c0aca75da8ef464eedfff4e3ae98c14a4
ANDREY-RANNEV <66748147+ANDREY-RANNEV@users.noreply.github.com>
2026-05-21 09:18:19 +0400

    Update connectiv.php

--- SamaraMap/app/connectiv.php
+++ SamaraMap/app/connectiv.php
@@ -1,14 +1,13 @@
 <?php
-$myUser = "sa";
-$myPass = "saadmin";
-// $server_name = "[2001:470:1f0b:3f7:5572:aa5a:4c71:a93e]";
+$myUser = "";
+$myPass = "";
+
 $server_name = "192.168.1.65";
 $database_name = "FBUZ";
 
-$_myUser= "rannev";
+$_myUser= "";
 $_myPass = "";
-// $_server_name = "[2001:470:1f0b:3f7:211:32ff:fe73:fb25]";
-// $_server_name = "[2001:470:1f0b:3f7:211:32ff:fe73:fb26]";
+
 $_server_name = "192.168.5.5";
 $_database_name = "FBUZ";
 $_port = '3307';
